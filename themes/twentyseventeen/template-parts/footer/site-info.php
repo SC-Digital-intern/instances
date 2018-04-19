f0a4455309858e1c75f06c848d6cd60989714d27
SC-Digital-intern <web_intern@storycorps.org>
2018-04-19 17:13:07 -0400

    added paragraph to footer

--- themes/twentyseventeen/template-parts/footer/site-info.php
+++ themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,6 @@
 
 ?>
 <div class="site-info">
+	<p>hello world</p>
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
 </div><!-- .site-info -->
