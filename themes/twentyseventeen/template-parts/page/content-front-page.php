e71a41029db5c0fabda494dc0366c9a23da91aaf
SC-Digital-intern <web_intern@storycorps.org>
2018-04-20 11:54:29 -0400

    Change header color

--- themes/twentyseventeen/template-parts/page/content-front-page.php
+++ themes/twentyseventeen/template-parts/page/content-front-page.php
@@ -27,7 +27,7 @@
 	<div class="panel-content">
 		<div class="wrap">
 			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				<?php the_title( '<h2 class="entry-title" style="color:blue;">', '</h2>' ); ?>
 
 				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
 
